68d7f19f3491f627dce5500f53ae55739029e507
Mike Judge <mikelovesrobots@gmail.com>
2024-07-14 14:51:57 -0700

    Disable gravity and add some initial momentum to the ball

--- 3hr-breakout/Assets/Scripts/Ball.cs
+++ 3hr-breakout/Assets/Scripts/Ball.cs
@@ -4,6 +4,19 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public float initialForce = 2f;
+
+    private void Start()
+    {
+        StartMoving();
+    }
+
+    public void StartMoving()
+    {
+        var randomDirection = new Vector2(Random.Range(-1f, 1f), 1).normalized;
+        GetComponent<Rigidbody2D>().velocity = randomDirection * 2f;
+    }
+
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("BottomWall"))
