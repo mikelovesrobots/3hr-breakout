93d7c3b2e49b896170ff83e90ed27ddd77397a83
Mike Judge <mikelovesrobots@gmail.com>
2024-07-14 14:34:38 -0700

    Add some 'english' to the paddle so when the ball hits off-center it adds an opposing force giving the player more control

--- 3hr-breakout/Assets/Scripts/Paddle.cs
+++ 3hr-breakout/Assets/Scripts/Paddle.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Callbacks;
 using UnityEngine;
 
-public class PaddleController : MonoBehaviour
+public class Paddle : MonoBehaviour
 {
     public float boundaryLeft = -8f;
     public float boundaryRight = 8f;
+    public float paddleImpactForce = 100f;
 
     void Update()
     {
@@ -13,5 +15,18 @@ public class PaddleController : MonoBehaviour
         var paddlePosition = new Vector3(mousePosition.x, transform.position.y, 0);
         paddlePosition.x = Mathf.Clamp(paddlePosition.x, boundaryLeft, boundaryRight);
         transform.position = paddlePosition;
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            float paddleCenter = transform.position.x;
+            float contactPoint = collision.GetContact(0).point.x;
+            float offset = contactPoint - paddleCenter;
+
+            var force = new Vector2(offset * paddleImpactForce, collision.rigidbody.velocity.y).normalized * paddleImpactForce;
+            collision.rigidbody.AddForce(force);
+        }
     }
 }
